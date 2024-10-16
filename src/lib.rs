6164add4189a10b25cdb8ac79ae578eaded0e20b
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-16 14:47:08 +0100

    fixes priority function in CoinbaseTipOrdering and adds tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,8 @@ pub trait TransactionOrdering<T: Transaction>: Send + Sync + 'static {
     ) -> Priority<Self::PriorityValue>;
 }
 
-pub enum Priority<T> {
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+pub enum Priority<T: Ord + Clone> {
     Value(T),
     None,
 }
@@ -48,7 +49,11 @@ where
         transaction: &T,
         base_fee: u64,
     ) -> Priority<Self::PriorityValue> {
-        transaction.effective_tip_per_gas(base_fee).map(U256::from).into()
+        // If the **effective** tip is zero, return Priority::None
+        transaction.effective_tip_per_gas(base_fee)
+            .map(U256::from)
+            .and_then(|tip| if tip.is_zero() {None} else {Some(tip)})
+            .map_or(Priority::None, Priority::Value)
     }
 }
 
@@ -69,8 +74,70 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_coinbase_tip_ordering_priority() {
-        
+    fn test_coinbase_tip_ordering_priority_with_default_fees() {
+        // Scenario: max_fee_priority_fee_per_gas == MockTransaction default value
+        // Expect: Priority::Value(expected)
+        let tx = MockTransaction::eip1559();
+        let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
+        let base_fee = 3;
+
+        let priority = ordering.priority(&tx, base_fee);
+        let expected = U256::from(tx.get_max_fee().unwrap() as u64 - base_fee);
+
+        assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
+    }
+
+    #[test]
+    fn test_coinbase_tip_ordering_zero_priority_fee() {
+        // Scenario: max_fee_priority_fee_per_gas == 0
+        // Expect: Priority::None since the **effective** tip is calculated (ie tip = min(max_fee - base_fee, max_priority_fee)) 
+        let tx = MockTransaction::eip1559().with_priority_fee(0);
+        let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
+        let base_fee = 3;
+
+        let priority = ordering.priority(&tx, base_fee);
+
+        assert_eq!(Priority::None, priority, "Expected Priority::None, got Priority::Value");
+    }
+
+    #[test]
+    fn test_coinbase_tip_ordering_max_priority_fee() {
+        // Scenario: max_fee_priority_fee_per_gas == u128::MAX
+        // Expect: Priority::Value(max_fee - base_fee) 
+        let tx = MockTransaction::eip1559().with_priority_fee(u128::MAX);
+        let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
+        let base_fee = 1;
+
+        let priority = ordering.priority(&tx, base_fee);
+        let expected = U256::from(tx.get_max_fee().unwrap() as u64 - base_fee);
+
+        assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
+    }
+
+    #[test]
+    fn test_coinbase_tip_ordering_base_fee_higher_than_max_fee() {
+        // Scenario: base_fee > max_fee_per_gas
+        // Expect: Priority::None since the **effective** tip is calculated (ie tip = min(max_fee - base_fee, max_priority_fee)) 
+        let tx = MockTransaction::eip1559().with_max_fee(0);
+        let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
+        let base_fee = 3;
+
+        let priority = ordering.priority(&tx, base_fee);
+        assert!(matches!(priority, Priority::None), "Expected Priority::None");
+    }
+
+    #[test]
+    fn test_coinbase_tip_ordering_max_fee() {
+        // Scenario: max_fee_priority_fee_per_gas == u128::MAX
+        // Expect: Priority::Value(max_fee - base_fee) 
+        let tx = MockTransaction::eip1559().with_max_fee(u128::MAX);
+        let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
+        let base_fee = 1;
+
+        let priority = ordering.priority(&tx, base_fee);
+        let expected = U256::from(tx.get_priority_fee().unwrap());
+
+        assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
     }
 }
 
@@ -125,14 +192,14 @@ pub enum MockTransaction {
 }
 
 impl MockTransaction {
-    pub fn eip1559(    ) -> Self {
+    pub fn eip1559() -> Self {
         Self::Eip1559{
             chain_id: 1,
             hash: B256::random(),
             sender: Address::random(),
             nonce: 0,
             max_fee_per_gas: 7,
-            max_priority_fee_per_gas: 7,
+            max_priority_fee_per_gas: 4,
             gas_limit: 0,
             to: Address::random().into(),
             value: Default::default(),
