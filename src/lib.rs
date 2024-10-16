a4ff4904036f1f753b489a920045d50f8e68d1dc
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-16 08:37:57 +0100

    adds CoinbaseTipOrdering and applies TransactionOrdering, Default and Clone traits

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,11 @@
 
 // -----ordering.rs-----
 
-use std::fmt;
-use alloy::consensus::Transaction;
+use std::{fmt, marker::PhantomData};
+use alloy::{
+    consensus::Transaction,
+    primitives::U256
+};
 
 pub trait TransactionOrdering<T: Transaction>: Send + Sync + 'static {
     
@@ -28,5 +31,35 @@ pub enum Priority<T> {
 impl<T: Ord + Clone> From<Option<T>> for Priority<T> {
     fn from(value: Option<T>) -> Priority<T> {
         value.map_or(Self::None, Priority::Value)
+    }
+}
+
+pub struct CoinbaseTipOrdering<T>(PhantomData<T>);
+
+impl<T> TransactionOrdering<T> for CoinbaseTipOrdering<T> 
+where   
+    T: Transaction + 'static,
+{
+    
+    type PriorityValue = U256;
+
+    fn priority(
+        &self,
+        transaction: &T,
+        base_fee: u64,
+    ) -> Priority<Self::PriorityValue> {
+        transaction.effective_tip_per_gas(base_fee).map(U256::from).into()
+    }
+}
+
+impl<T> Default for CoinbaseTipOrdering<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl<T> Clone for CoinbaseTipOrdering<T> {
+    fn clone(&self) -> Self {
+        Self::default()
     }
 }
