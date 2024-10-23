ccd1d34b98496672cf5664012a37bc1379aa1979
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-23 11:03:02 +0100

    moves PoolUpdate structs under result.rs section

--- src/lib.rs
+++ src/lib.rs
@@ -726,33 +726,6 @@ impl AllTransactions
 }
 
 
-
-// -----updates.rs
-
-// A change of the transaction's location
-///
-/// NOTE: this guarantees that `current` and `destination` differ.
-#[derive(Debug)]
-pub(crate) struct PoolUpdate {
-    /// Internal tx id.
-    pub(crate) id: TransactionId,
-    /// Hash of the transaction.
-    pub(crate) hash: TxHash,
-    /// Where the transaction is currently held.
-    pub(crate) current: SubPool,
-    /// Where to move the transaction to.
-    pub(crate) destination: Destination,
-}
-
-/// Where to move an existing transaction.
-#[derive(Debug)]
-pub(crate) enum Destination {
-    /// Discard the transaction.
-    Discard,
-    /// Move transaction to pool
-    Pool(SubPool),
-}
-
 // -----pending.rs-----
 
 #[derive(Eq, PartialEq)]
@@ -1313,6 +1286,30 @@ impl<T> Clone for CoinbaseTipOrdering<T> {
 // -----result.rs-----
 
 use thiserror;
+
+// A change of the transaction's location
+///
+/// NOTE: this guarantees that `current` and `destination` differ.
+#[derive(Debug)]
+pub(crate) struct PoolUpdate {
+    /// Internal tx id.
+    pub(crate) id: TransactionId,
+    /// Hash of the transaction.
+    pub(crate) hash: TxHash,
+    /// Where the transaction is currently held.
+    pub(crate) current: SubPool,
+    /// Where to move the transaction to.
+    pub(crate) destination: Destination,
+}
+
+/// Where to move an existing transaction.
+#[derive(Debug)]
+pub(crate) enum Destination {
+    /// Discard the transaction.
+    Discard,
+    /// Move transaction to pool
+    Pool(SubPool),
+}
 
 pub(crate) type InsertResult = Result<InsertOk, InsertErr>;
 
