515c63abc21c1a8c732da4de098ec3194e75cfbb
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-24 09:44:34 +0100

    adds tests for ordering.rs

--- src/ordering.rs
+++ src/ordering.rs
@@ -67,74 +67,67 @@ impl<T> Clone for CoinbaseTipOrdering<T> {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::helpers::create_tx_and_sender;
+    use alloy::primitives::U256;
 
-//     #[test]
-//     fn test_coinbase_tip_ordering_priority_with_default_fees() {
-//         // Scenario: max_fee_priority_fee_per_gas == MockTransaction default value
-//         // Expect: Priority::Value(expected)
-//         let tx = MockTransaction::eip1559();
-//         let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
-//         let base_fee = 3;
+    #[tokio::test]  
+    async fn test_coinbase_tip_ordering_priority_with_default_fees() {
+        let (tx, _, _) = create_tx_and_sender(100, 50, 1000, U256::from(0), 0).await;
+        let ordering = CoinbaseTipOrdering::<TxEnvelope>::default();
+        let base_fee = 40;
 
-//         let priority = ordering.priority(&tx, base_fee);
-//         let expected = U256::from(tx.get_max_fee().unwrap() as u64 - base_fee);
+        // effective tip = min(max_fee_per_gas - base_fee, max_priority_fee_per_gas)
+        // effective tx1_tip = min(100 - 40, 50) = 50
+        let priority = ordering.priority(&tx, base_fee);
+        let expected = U256::from(50);
 
-//         assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
-//     }
+        assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value(50)");
+    }
+    #[tokio::test]
+    async fn test_coinbase_tip_ordering_zero_priority_fee() {
+        let (tx, _, _) = create_tx_and_sender(100, 0, 1000, U256::from(0), 0).await;
+        let ordering = CoinbaseTipOrdering::<TxEnvelope>::default();
+        let base_fee = 40;
 
-//     #[test]
-//     fn test_coinbase_tip_ordering_zero_priority_fee() {
-//         // Scenario: max_fee_priority_fee_per_gas == 0
-//         // Expect: Priority::None since the **effective** tip is calculated (ie tip = min(max_fee - base_fee, max_priority_fee)) 
-//         let tx = MockTransaction::eip1559().with_priority_fee(0);
-//         let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
-//         let base_fee = 3;
+        let priority = ordering.priority(&tx, base_fee);
 
-//         let priority = ordering.priority(&tx, base_fee);
+        assert_eq!(priority, Priority::None, "Expected Priority::None");
+    }
 
-//         assert_eq!(Priority::None, priority, "Expected Priority::None, got Priority::Value");
-//     }
+    #[tokio::test]
+    async fn test_coinbase_tip_ordering_max_priority_fee() {
+        let (tx, _, _) = create_tx_and_sender(100, u128::MAX, 1000, U256::from(0), 0).await;
+        let ordering = CoinbaseTipOrdering::<TxEnvelope>::default();
+        let base_fee = 40;
 
-//     #[test]
-//     fn test_coinbase_tip_ordering_max_priority_fee() {
-//         // Scenario: max_fee_priority_fee_per_gas == u128::MAX
-//         // Expect: Priority::Value(max_fee - base_fee) 
-//         let tx = MockTransaction::eip1559().with_priority_fee(u128::MAX);
-//         let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
-//         let base_fee = 1;
+        let priority = ordering.priority(&tx, base_fee);
+        let expected = U256::from(60); // 100 - 40
 
-//         let priority = ordering.priority(&tx, base_fee);
-//         let expected = U256::from(tx.get_max_fee().unwrap() as u64 - base_fee);
+        assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value(60)");
+    }
 
-//         assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
-//     }
+    #[tokio::test]
+    async fn test_coinbase_tip_ordering_base_fee_higher_than_max_fee() {
+        let (tx, _, _) = create_tx_and_sender(100, 50, 1000, U256::from(0), 0).await;
+        let ordering = CoinbaseTipOrdering::<TxEnvelope>::default();
+        let base_fee = 110;
 
-//     #[test]
-//     fn test_coinbase_tip_ordering_base_fee_higher_than_max_fee() {
-//         // Scenario: base_fee > max_fee_per_gas
-//         // Expect: Priority::None since the **effective** tip is calculated (ie tip = min(max_fee - base_fee, max_priority_fee)) 
-//         let tx = MockTransaction::eip1559().with_max_fee(0);
-//         let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
-//         let base_fee = 3;
+        let priority = ordering.priority(&tx, base_fee);
 
-//         let priority = ordering.priority(&tx, base_fee);
-//         assert!(matches!(priority, Priority::None), "Expected Priority::None");
-//     }
+        assert_eq!(priority, Priority::None, "Expected Priority::None");
+    }
 
-//     #[test]
-//     fn test_coinbase_tip_ordering_max_fee() {
-//         // Scenario: max_fee_priority_fee_per_gas == u128::MAX
-//         // Expect: Priority::Value(max_fee - base_fee) 
-//         let tx = MockTransaction::eip1559().with_max_fee(u128::MAX);
-//         let ordering: CoinbaseTipOrdering<MockTransaction> = CoinbaseTipOrdering::default();
-//         let base_fee = 1;
+    #[tokio::test]
+    async fn test_coinbase_tip_ordering_base_fee_equal_to_max_fee() {
+        let (tx, _, _) = create_tx_and_sender(100, 50, 1000, U256::from(0), 0).await;
+        let ordering = CoinbaseTipOrdering::<TxEnvelope>::default();
+        let base_fee = 100;
 
-//         let priority = ordering.priority(&tx, base_fee);
-//         let expected = U256::from(tx.get_priority_fee().unwrap());
+        let priority = ordering.priority(&tx, base_fee);
 
-//         assert_eq!(priority, Priority::Value(expected), "Expected Priority::Value");
-//     }
-// }
+        assert_eq!(priority, Priority::None, "Expected Priority::None");
+    }
+}
