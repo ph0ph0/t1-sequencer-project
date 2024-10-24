9b0cf5c72fab5b94ae8e2577d6730ea625487990
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-24 10:16:59 +0100

    removes unnecessary trait bounds on Transaction ordering and adds new() function to sequence.rs

--- src/pool/sequence.rs
+++ src/pool/sequence.rs
@@ -10,10 +10,19 @@ use crate::{
 
 pub struct TransactionSequence<O>
 where
-    O: TransactionOrdering + PartialEq + Eq + PartialOrd + Ord,
+    O: TransactionOrdering,
 {
     ordering: O,
     sequenced_transactions: BTreeSet<PendingTransaction<O>>,
     // nonce_map: HashMap<u64, u64>, //TODO: Remove
     sum_priority_fee: u128,
+}
+
+impl<O> TransactionSequence<O>
+where
+    O: TransactionOrdering,
+{
+    pub fn new(ordering: O) -> Self {
+        Self { ordering, sequenced_transactions: BTreeSet::default(), sum_priority_fee: 0 }
+    }
 }
