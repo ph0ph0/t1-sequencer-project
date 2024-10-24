91d70996eb8811b0db2756080472794059f87d6c
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-10-24 10:46:09 +0100

    adds struct properties to TransactionSequence

--- src/pool/sequence.rs
+++ src/pool/sequence.rs
@@ -1,28 +1,34 @@
 // -----sequence.rs-----
 
-use std::collections::BTreeSet
-;
+use std::collections::{BTreeMap, BTreeSet, HashSet};
+
+use tokio::sync::mpsc::Receiver;
+use alloy::primitives::TxHash;
 
 use crate::{
     ordering::TransactionOrdering,
     pool::pending::PendingTransaction,
+    identifiers::TransactionId,
 };
 
 pub struct TransactionSequence<O>
 where
     O: TransactionOrdering,
 {
-    ordering: O,
-    sequenced_transactions: BTreeSet<PendingTransaction<O>>,
-    // nonce_map: HashMap<u64, u64>, //TODO: Remove
-    sum_priority_fee: u128,
-}
-
-impl<O> TransactionSequence<O>
-where
-    O: TransactionOrdering,
-{
-    pub fn new(ordering: O) -> Self {
-        Self { ordering, sequenced_transactions: BTreeSet::default(), sum_priority_fee: 0 }
-    }
+    /// Contains a copy of _all_ transactions of the pending pool at the point in time this
+    /// iterator was created.
+    pub(crate) all: BTreeMap<TransactionId, PendingTransaction<O>>,
+    /// Transactions that can be executed right away: these have the expected nonce.
+    ///
+    /// Once an `independent` transaction with the nonce `N` is returned, it unlocks `N+1`, which
+    /// then can be moved from the `all` set to the `independent` set.
+    pub(crate) independent: BTreeSet<PendingTransaction<O>>,
+    /// There might be the case where a yielded transactions is invalid, this will track it.
+    pub(crate) invalid: HashSet<TxHash>,
+    /// Used to receive any new pending transactions that have been added to the pool after this
+    /// iterator was static fileted
+    ///
+    /// These new pending transactions are inserted into this iterator's pool before yielding the
+    /// next value
+    pub(crate) new_transaction_receiver: Option<Receiver<PendingTransaction<O>>>,
 }
